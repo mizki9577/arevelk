5ceec6c602798f21d13471668d41d0b8828caeae
Mizki SUZUMORI <mizki9577@gmail.com>
2017-05-21 00:28:14 +0900

    randomChoice: make it throw an exception when given array was empty

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 // @flow
 
 export const randomChoice = (array: any[], destructive: boolean=false) => {
+  if (array.length === 0) throw 'given array was empty.'
   const i = Math.floor(Math.random() * array.length)
   if (destructive) return array.splice(i, 1)[0]
   return array[i]
