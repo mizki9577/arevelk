d90ebd4e8537a5923a857a0d34b2c1267baa9682
Mizki SUZUMORI <mizki9577@gmail.com>
2017-05-26 00:56:37 +0900

    get it functionally

--- src/utils.js
+++ src/utils.js
@@ -6,71 +6,47 @@ import Triangle from './Triangle.js'
 import Point from './Point.js'
 
 export const delaunayTriangulate = (points: Point[]) => {
-  const createSuperTriangle = (points: Point[]) => {
-    const max_x = math.max(points.map(p => p.x))
-    const max_y = math.max(points.map(p => p.y))
-    const min_x = math.min(points.map(p => p.x))
-    const min_y = math.min(points.map(p => p.y))
-
-    // create a isosceles right triangle
-    const p1 = new Point(min_x - 1    , min_y - 1)      // bottom left
-    const p2 = new Point(max_x * 2 + 2, min_y - 1)      // bottom right
-    const p3 = new Point(min_x - 1    , max_y * 2 + 2)  // top left
-    return new Triangle(p1, p2, p3)
-  }
-
-  const triangles = []
+  let triangles = []
   const super_triangle = createSuperTriangle(points)
   triangles.push(super_triangle)
 
   for (let point of points) {
-    const bad_triangles = []
+    const bad_triangles = triangles.filter(t => t.circumcircleContainsPoint(point))
 
-    for (const triangle of triangles) {
-      if (triangle.circumcircleContainsPoint(point)) {
-        bad_triangles.push(triangle)
-      }
-    }
+    const polygon = bad_triangles.map(t => t.getEdges())
+                                 .reduce((prev, next) => prev.concat(next))
+                                 .filter((_, i, array) => isUnique(array, i, (e1, e2) => e1.isEqual(e2)))
 
-    const polygon = []
-    for (const bad_triangle of bad_triangles) {
-      for (const bad_edge of bad_triangle.getEdges()) {
-        let edge_is_shared = false
+    const good_triangles = triangles.filter(t => !t.circumcircleContainsPoint(point))
 
-        for (const other_bad_triangle of bad_triangles) {
-          if (bad_triangle.isEqual(other_bad_triangle)) continue
+    const new_triangles = polygon.map(e => new Triangle(point, e.begin, e.end))
 
-          if (other_bad_triangle.hasEdge(bad_edge)) {
-            edge_is_shared = true
-            break
-          }
-        }
-
-        if (!edge_is_shared) {
-          polygon.push(bad_edge)
-        }
-      }
-    }
-
-    for (const bad_triangle of bad_triangles) {
-      const i = triangles.findIndex(t => t.isEqual(bad_triangle))
-      if (i === -1) continue
-      triangles.splice(i, 1)
-    }
-
-    for (const edge of polygon) {
-      triangles.push(new Triangle(edge.begin, point, edge.end))
-    }
+    triangles = [...good_triangles, ...new_triangles]
   }
 
   for (const super_vertex of super_triangle) {
-    const i = triangles.findIndex(t => t.findIndex(v => v.isEqual(super_vertex)) !== -1)
-    if (i === -1) continue
-    triangles.splice(i, 1)
+    triangles = triangles.filter(t => t.findIndex(v => v.isEqual(super_vertex)) === -1)
   }
 
   return triangles
 }
+
+const createSuperTriangle = (points: Point[]) => {
+  const max_x = math.max(points.map(p => p.x))
+  const max_y = math.max(points.map(p => p.y))
+  const min_x = math.min(points.map(p => p.x))
+  const min_y = math.min(points.map(p => p.y))
+
+  // create a isosceles right triangle
+  const p1 = new Point(min_x - 1    , min_y - 1)      // bottom left
+  const p2 = new Point(max_x * 2 + 2, min_y - 1)      // bottom right
+  const p3 = new Point(min_x - 1    , max_y * 2 + 2)  // top left
+  return new Triangle(p1, p2, p3)
+}
+
+const isUnique = (array: any[], index: number, fn: (any, any) => boolean): boolean => (
+  array.slice(0, index).findIndex(v => fn(v, array[index])) === -1 && array.slice(index + 1).findIndex(v => fn(v, array[index])) === -1
+)
 
 export const randomDelaunayTriangulation = (polygon: Polygon, numberOfSplits: number): Triangle[] => {
   const points = Array.from(polygon)
