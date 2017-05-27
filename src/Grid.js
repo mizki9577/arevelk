9e9f09a53e2f5d338e2cc94e643369cbec099849
Mizki SUZUMORI <mizki9577@gmail.com>
2017-05-27 19:06:23 +0900

    fuck flow

--- src/Grid.js
+++ src/Grid.js
@@ -1,12 +1,23 @@
-// @flow
+// TODO: flow
 import React from 'react'
 
-export default ({ x, y, width, height, r=0.1, className=null }) => {
+type Props = {
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  r: ?number,
+  className: ?string,
+}
+
+const Grid = ({ x, y, width, height, r=0.1, className='' }: Props) => {
   const grid = []
   for (let i = 0; i < width; ++i) for (let j = 0; j < height; ++j) {
     grid.push(<circle key={ `grid${i},${j}` } cx={ i } cy={ j } r={ r } />)
   }
   return <g className={ className }>{ grid }</g>
 }
+
+export default Grid
 
 // vim: set ts=2 sw=2 et:
